ef9483ee55798246b4d991d050555f3f2d0a8cfd
JamesAri <James.Ari@outlook.com>
2025-05-23 17:45:02 +0200

    rsys

--- rsyslog/rsyslog.sh
+++ rsyslog/rsyslog.sh
@@ -17,7 +17,7 @@ iptables -I INPUT -j ACCEPT -p tcp --dport 514
 
 vim /etc/rsyslog.d/10-remote.conf
 
-# =======
+# =================================================
 
 $template RemoteLogs,"/var/log/remote/%HOSTNAME%/%PROGRAMNAME%.log" *.* ?RemoteLogs
 $template HourlyMailLog,"/var/log/logdir/%$YEAR%/%$MONTH%/%$DAY%/%HOSTNAME%_mail.log
@@ -26,10 +26,11 @@ $template SyslFormat,"%timegenerated% %HOSTNAME%  %syslogtag%%msg:::space$
 # zapis logu do souboru dle definice a formatu
 mail.*                                                  -?HourlyMailLog;SyslFormat
 
-# =======
+# =================================================
 
-sudo mkdir -p /var/log/remote
-sudo chown syslog:adm /var/log/remote
+sudo mkdir -p /var/log/errors
+# sudo chown syslog:adm /var/log/remote
+sudo chown syslog:adm /var/log/errors
 sudo systemctl restart rsyslog
 
 # CLIENT
@@ -46,13 +47,21 @@ sudo find /var/log/remote -type f
 sudo tail -f /var/log/remote/*/*.log
 
 
+
+
+# template(name="RemoteLogs" type="string" string="/var/log/remote/%HOSTNAME%/%PROGRAMNAME%.log")
+# action(type="omfile" dynaFile="RemoteLogs" createDirs="on" DirCreateMode="0755" sync="off")
+
+# ============ APACHE LOGGING ============
+# == Legacy rsyslog syntax ==
+
 # /etc/rsyslog.d/apache.conf
 local6.err      /var/log/apache/httpd-error.log
 local6.notice   /var/log/apache/httpd-access.log
 # nebo na remote
 local6.*        @192.168.1.100:514
 
-
+# == RainerScript syntax ==
 # /etc/rsyslog.d/apache.conf
 if $syslogfacility-text == 'local6' and $programname == 'httpd' and ($syslogseverity-text == 'err' or $syslogseverity-text == 'notice') then /var/log/rsyslog-apache-log.log
 & stop
@@ -61,21 +70,38 @@ if $syslogfacility-text == 'local6' and $programname == 'httpd' and ($syslogseve
 errorlog  "|/usr/bin/tee -a /var/log/www/error.log  | /usr/bin/logger -t httpd -p local6.err"
 customlog "|/usr/bin/tee -a /var/log/www/access.log | /usr/bin/logger -t httpd -p local6.notice" extended_ncsa
 
-
-# $ActionFileEnableSync off
-# $ActionFileDefaultTemplate SyslFormat
-# $WorkDirectory /var/spool/rsyslog
-# $CreateDirs on
-
-
 # module(load="imudp")    # pokud přijímáš UDP logy
 # module(load="imtcp")    # pokud přijímáš TCP logy
 
-# template(name="RemoteLogs" type="string"
-#          string="/var/log/remote/%HOSTNAME%/%PROGRAMNAME%.log")
+# ====== LOGGING ======
 
-# action(type="omfile"
-#        dynaFile="RemoteLogs"
-#        createDirs="on"
-#        DirCreateMode="0755"
-#        sync="off")
+# RainerScript syntax
+# 'error'
+# 'critical'
+# 'warning'
+# 'information'
+
+# Legacy rsyslog syntax
+# emerg
+# alert
+# crit
+# err
+# warning
+# notice
+# info
+# debug
+
+cat > /etc/rsyslog.d/50-error-logs.conf << 'EOF'
+template(name="ErrorLogPath" type="string" string="/var/log/errors/%$YEAR%/%$MONTH%/%$DAY%/errors.log")
+if $syslogseverity-text == 'error' then {
+    action(type="omfile" dynaFile="ErrorLogPath" createDirs="on" DirCreateMode="0755" sync="off")
+    stop
+}
+EOF
+systemctl restart rsyslog
+logger -p user.error "This is a test error message"
+cd /var/log/errors
+
+# mkdir -p /var/log/errors
+# chown root:adm /var/log/errors
+
