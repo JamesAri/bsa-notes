f29b8e448fe123c1a0c9ef5a244986b765519e94
JamesAri <James.Ari@outlook.com>
2025-05-25 23:50:34 +0200

    asdad

--- user/user.sh
+++ user/user.sh
@@ -13,3 +13,41 @@ sed -i 's/PasswordAuthentication .*/PasswordAuthentication no/g' /etc/ssh/sshd_c
 systemctl restart sshd
 chmod 755 /home/${USER_NM}
 chown ${USER_NM}:${USER_NM} /home/${USER_NM}
+
+
+# ======================================================
+# -m to create home
+# -s for shell
+# -d specify home directory
+# -g to specify group
+# -r system acc
+useradd -m -s /bin/bash karel
+
+# set password for user
+passwd karel
+
+# checks
+id karel
+getent passwd karel
+
+# create group
+addgroup nginx_admins
+
+# add user to group
+# -a to append user to group
+# -G to specify group
+usermod -aG nginx_admins karel
+
+
+# SUDO for user
+usermod -aG sudo karel
+echo "karel  ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/karel
+
+# allow nginx_admins to reset nginx
+echo "%nginx_admins ALL=(ALL) NOPASSWD: /bin/systemctl restart nginx" > /etc/sudoers.d/nginx_admins
+
+
+
+
+
+
